08c24d4357845ed70bc67ed4cbd5ac2e06c24125
misener7 <xqingmiao@126.com>
2023-06-16 17:21:00 +0800

    新增GPT3.5对话调用

--- README.md
+++ README.md
@@ -8,5 +8,8 @@
 ## 示例
 ![截图](png/example1.png)
 
+## 新增
+增加通过token调用GPT对话的方式，可以通过token,通过构建本地知识库的方式调用GPT对话。
+
 ## LICENSE
 <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="知识共享许可协议" style="border-width:0" src="https://img.shields.io/badge/license-CC%20BY--NC--SA%204.0-lightgrey" /></a><br />本作品采用<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">知识共享署名-非商业性使用-相同方式共享 4.0 国际许可协议</a>进行许可。
--- web_demo2.py
+++ web_demo2.py
@@ -1,4 +1,5 @@
 import os
+
 import streamlit_chat
 import streamlit as st
 from langchain.chains import RetrievalQA
@@ -10,6 +11,10 @@ import sentence_transformers
 
 from tool.chatllm import ChatLLM
 from tool.chinese_text_splitter import ChineseTextSplitter
+from tool.pandora_with_langchain_llm import *
+from typing import Callable, Optional
+
+chain: Optional[Callable] = None
 
 import torch
 LLM_DEVICE = "cuda" if torch.cuda.is_available(
@@ -27,7 +32,7 @@ class KnowledgeBasedChatLLM:
     llm: object = None
     embeddings: object = None
 
-    def init_model_config(self):
+    def init_model_config(self,model_type):
 
         self.embeddings = HuggingFaceEmbeddings(
             model_name="/data/tool/text2vec-base-chinese", )
@@ -36,30 +41,31 @@ class KnowledgeBasedChatLLM:
             device=EMBEDDING_DEVICE,
             cache_folder=os.path.join(MODEL_CACHE_PATH,
                                       self.embeddings.model_name))
-        self.llm = ChatLLM()
 
-        self.llm.model_type = 'chatglm'
-        self.llm.model_name_or_path = "./chatglm-6b"
-        self.llm.load_llm(llm_device=LLM_DEVICE)
+        if model_type == 'chatglm':
+            self.llm = ChatLLM()
+
+            self.llm.model_type = 'chatglm'
+            self.llm.model_name_or_path = "./chatglm-6b"
+            self.llm.load_llm(llm_device=LLM_DEVICE)
+        elif model_type == 'gpt':
+            self.llm = CustomChatGPT(base_url="http://192.168.12.24:8008/")
+
 
     def init_knowledge_vector_store(self, filepath):
 
         docs = self.load_file(filepath)
-        print("docs", docs)
-        print(self.embeddings)
         vector_store = FAISS.from_documents(docs, self.embeddings)
         vector_store.save_local('faiss_index')
         return vector_store
 
-    def get_knowledge_based_answer(self,
+    def get_knowledge_based_answer(self,model_type,
                                    query,
                                    top_k: int = 6,
                                    history_len: int = 3,
                                    temperature: float = 0.01,
                                    top_p: float = 0.1,
                                    history=[]):
-        self.llm.temperature = temperature
-        self.llm.top_p = top_p
         self.history_len = history_len
         self.top_k = top_k
 
@@ -71,30 +77,50 @@ class KnowledgeBasedChatLLM:
 
             问题:
             {question}"""
+
         prompt = PromptTemplate(template=prompt_template,
                                 input_variables=["context", "question"])
-        self.llm.history = history[
-            -self.history_len:] if self.history_len > 0 else []
 
         vector_store = FAISS.load_local('faiss_index', self.embeddings)
 
-        knowledge_chain = RetrievalQA.from_llm(
-            llm=self.llm,
-            retriever=vector_store.as_retriever(
-                search_kwargs={"k": self.top_k}),
-            prompt=prompt)
-        knowledge_chain.combine_documents_chain.document_prompt = PromptTemplate(
-            input_variables=["page_content"], template="{page_content}")
+        if model_type == 'chatglm':
+            self.llm.temperature = temperature
+            self.llm.top_p = top_p
+            self.llm.history = history[
+                               -self.history_len:] if self.history_len > 0 else []
+            knowledge_chain = RetrievalQA.from_llm(
+                llm=self.llm,
+                retriever=vector_store.as_retriever(
+                    search_kwargs={"k": self.top_k}),
+                prompt=prompt)
+            knowledge_chain.combine_documents_chain.document_prompt = PromptTemplate(
+                input_variables=["page_content"], template="{page_content}")
 
-        knowledge_chain.return_source_documents = True
+            knowledge_chain.return_source_documents = True
 
-        result = knowledge_chain({"query": query})
+            result = knowledge_chain({"query": query})
+        elif model_type == 'gpt':
+            knowledge_chain = RetrievalQA.from_llm(
+                llm=self.llm,
+                retriever=vector_store.as_retriever(
+                    search_kwargs={"k": self.top_k}),
+                prompt=prompt)
+            knowledge_chain.combine_documents_chain.document_prompt = PromptTemplate(
+                input_variables=["page_content"], template="{page_content}")
+
+            knowledge_chain.return_source_documents = True
+            result = knowledge_chain({"query": query})
+            print(result)
         return result
 
     def load_file(self, filepath):
         if filepath.lower().endswith(".md"):
             loader = UnstructuredFileLoader(filepath, mode="elements")
             docs = loader.load()
+        elif filepath.lower().endswith(".pdf"):
+            loader = UnstructuredFileLoader(filepath)
+            textsplitter = ChineseTextSplitter(pdf=True)
+            docs = loader.load_and_split(textsplitter)
         else:
             loader = UnstructuredFileLoader(filepath, mode="elements")
             textsplitter = ChineseTextSplitter(pdf=False)
@@ -102,23 +128,23 @@ class KnowledgeBasedChatLLM:
         return docs
 
 @st.cache_resource
-def init_model():
+def init_model(model_type):
+    print("init_model")
     knowladge_based_chat_llm = KnowledgeBasedChatLLM()
-    knowladge_based_chat_llm.init_model_config()
-    knowladge_based_chat_llm.llm._call("你好")
+    knowladge_based_chat_llm.init_model_config(model_type= model_type)
     return knowladge_based_chat_llm
 
 
 MAX_TURNS = 20
 MAX_BOXES = MAX_TURNS * 2
 
-def predict(knowladge_based_chat_llm,input,
+def predict(model_type,knowladge_based_chat_llm,input,
             top_k,
             history_len,
             temperature,
             top_p,
             history=None):
-    print(input,top_k,history_len,temperature,top_p,history)
+
     if history == None:
         history = []
 
@@ -133,6 +159,7 @@ def predict(knowladge_based_chat_llm,input,
         streamlit_chat.message(input, avatar_style="big-smile", key=str(len(history)) + "_user")
         st.write("AI正在回复:")
         resp = knowladge_based_chat_llm.get_knowledge_based_answer(
+            model_type=model_type,
             query=input,
             top_k=top_k,
             history_len=history_len,
@@ -151,13 +178,17 @@ def init_vector_store(file_path,knowladge_based_chat_llm):
         file_path)
     return vector_store
 
-knowladge_based_chat_llm = init_model()
 container = st.container()
 
 prompt_text = st.text_area(label="用户命令输入",
             height = 100,
             placeholder="请在这儿输入您的命令")
 
+model_type = st.sidebar.radio(
+    "模型类型",
+    ('gpt','chatglm'))
+
+# model_type = 'chatglm'
 top_k = st.sidebar.slider(
     'top_k', 1, 10, 6, step=1
 )
@@ -174,8 +205,9 @@ temperature = st.sidebar.slider(
     'temperature', 0.0, 1.0, 0.95, step=0.01
 )
 
-data = st.sidebar.file_uploader("上传文本文件", type=["txt", "md", "docx"])
+data = st.sidebar.file_uploader("上传文本文件", type=["txt", "md", "pdf", "docx"])
 init_vs = st.sidebar.button("知识库文件向量化")
+knowladge_based_chat_llm = init_model(model_type=model_type)
 
 if init_vs:
     if data is not None:
@@ -194,4 +226,4 @@ if 'state' not in st.session_state:
     st.session_state['state'] = []
 if st.button("发送", key="predict"):
     with st.spinner("AI正在思考，请稍等........"):
-        st.session_state["state"] = predict(knowladge_based_chat_llm,prompt_text, top_k,history_len, temperature, top_p, st.session_state["state"])
+        st.session_state["state"] = predict(model_type,knowladge_based_chat_llm,prompt_text, top_k,history_len, temperature, top_p, st.session_state["state"])
